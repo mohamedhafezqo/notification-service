dafef1e12d97c2885267b6a16977a5a618928caf
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-05-31 04:11:21 +0200

    Added channel constant

---
+++ src/Constant/ChannelTypeConstant.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Constant;
+
+/**
+ * Class ChannelTypeConstant
+ *
+ * @package App\Constant
+ */
+final class ChannelTypeConstant
+{
+    const EMAIL = 'email';
+    const SMS = 'sms';
+    const PUSH_NOTIFICATION = 'push_notitfication';
+}
